978c678b6b5e4183779e86c911c97079f20cb296
Edivan de Castro <edivandecastro@gmail.com>
2022-08-28 11:11:59 -0300

    Created FullLayout

---
+++ src/layouts/FullLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/dashboard"> Dashboard </router-link> |
+      <router-link to="/aportes">Aportes</router-link> |
+      <router-link to="/proventos">Proventos </router-link> | <router-link to="/ativos">ativos</router-link> |
+      <router-link to="/corretoras">Corretoras</router-link> |
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FullLayout",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
