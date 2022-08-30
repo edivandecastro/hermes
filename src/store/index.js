32601af6d83fca3ba6f010f719e322c6635fab9f
Edivan de Castro <edivandecastro@gmail.com>
2022-08-30 19:45:15 -0300

    Added strict mode in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import User from '../store/modules/user.module'
+import User from './modules/user.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  strict: true,
+  strict: process.env.NODE_ENV !== 'production',
   modules: {
     User
   }
